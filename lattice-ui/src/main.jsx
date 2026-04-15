00bfe9ad19731f864fca44a1819a9d411831bcf1
Glasik <glasik@openclaw>
2026-04-14 19:19:04 -0500

    wire: port 5174 to AppMultiTab with Glasik/GN tabs

--- lattice-ui/src/main.jsx
+++ lattice-ui/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppMultiTab from './AppMultiTab.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppMultiTab />
   </StrictMode>,
 )
